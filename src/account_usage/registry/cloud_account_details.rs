1ff416f7bbbe3e0e74ab6162d5874e550f84407c
sn00m4n <ageissler@trufflepig-forensics.de>
2023-11-22 11:27:07 +0100

    removed unused .rs files

--- src/account_usage/registry/cloud_account_details.rs
+++
@@ -1,1 +0,0 @@
-// "Microsoft Cloud Accounts store account information in the SAM hive, including the email address associated with the account" - SANS Windows Forensic Analysis Poster, Cloud Account Details
